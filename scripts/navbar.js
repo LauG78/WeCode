ad410f9e4f2df6405feca0ece204bb1fc576e644
Yasser <yasserbal@icloud.com>
2022-04-30 20:41:50 +0200

    Updated to hide mobile nav if nav is not on top

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector('.burger__container');
 const navList = document.querySelector('.global__nav__list');
-
+var screenWidth = document.documentElement.clientWidth;
+let globalNav = document.querySelector('.global__nav');
 let menuOpen = false;
-
+let navStickyOnTop = false;
 burger.addEventListener('click',triggerNav)
-
+triggerOnResize();
+window.addEventListener('resize', triggerOnResize)
 function triggerNav() {
     if (menuOpen == false) {
         navList.classList.add('active')
@@ -14,5 +16,28 @@ function triggerNav() {
         navList.classList.remove('active')
         burger.classList.remove('active')
         menuOpen = false;
+    }
+}
+function triggerOnResize() {
+    if (screenWidth < 769) {
+        window.addEventListener('scroll', checkNavPosition)
+    }
+}
+function checkNavPosition() {
+    if (globalNav.getBoundingClientRect().top <= 1) {
+        navStickyOnTop = true;
+        toggleNavList();
+    } else if (globalNav.getBoundingClientRect().top >= 1) {
+        navStickyOnTop = false;
+        toggleNavList();
+    }
+}
+function toggleNavList() {
+    if (!navStickyOnTop) {
+        navList.classList.add('display__none');
+        burger.classList.add('display__none');
+    } else if (navStickyOnTop) {
+        navList.classList.remove('display__none');
+        burger.classList.remove('display__none');
     }
 }
