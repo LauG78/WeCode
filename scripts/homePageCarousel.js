189562fa7119935a549df1434e80065785b879f1
Yasser <yasserbal@icloud.com>
2022-05-01 03:40:44 +0200

    Working on Welcome btn js

--- scripts/homePageCarousel.js
+++ scripts/homePageCarousel.js
@@ -1,14 +1,22 @@
 const carBtnPrev = document.querySelector('.home__carousel__button__container__prev');
 const carBtnNext = document.querySelector('.home__carousel__button__container__next');
+const carBtnWelcome = document.querySelector('.welcome__button')
+const vh = document.documentElement.clientHeight;
+let navHeight = globalNav.offsetHeight;
 carBtnNext.addEventListener('click', nextSlide);
 carBtnPrev.addEventListener('click', prevSlide);
+carBtnWelcome.addEventListener('click',scrollToMain)
 const carSlideshow = document.querySelector('.home__carousel__slideshow')
 var carSlides = carSlideshow.children;
 var carSlideIndex = 0;
 var carAutoPlay = setInterval(autoplayCarSlides, 8000)
+function scrollToMain(){
+    scrollTo({
+        top:vh-navHeight,behavior:'smooth'
+    })
+}
 function nextSlide() {
     if (carSlideIndex < carSlides.length - 1) {
-
         carSlides[carSlideIndex].classList.remove('home__carousel__active__slide')
         carSlideIndex = carSlideIndex + 1;
         carSlides[carSlideIndex].classList.add('home__carousel__active__slide')
